2c4c468c8595423969e26ed2e6fc4a32eaf0a3d0
pannu <pann00008@alognquinlive.com>
2018-03-20 16:02:21 -0400

    All the new changes

--- src/UI/ProfessorController.java
+++ src/UI/ProfessorController.java
@@ -12,15 +12,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import Domain.User;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+
 
 
 public class ProfessorController implements Initializable {
